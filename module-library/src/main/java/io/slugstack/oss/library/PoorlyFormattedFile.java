2dc9ef020fbd0dd659b13a649777eaa32e860f67
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2021-04-14 13:46:28 -0500

    poorly formatted file for AutoFormat to mess with (#4)

---
+++ module-library/src/main/java/io/slugstack/oss/library/PoorlyFormattedFile.java
@@ -0,0 +1,41 @@
+package io.slugstack.oss.library;
+
+// this class is poorly formatted and should be picked up by rewrite AutoFormat
+public class PoorlyFormattedFile {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
