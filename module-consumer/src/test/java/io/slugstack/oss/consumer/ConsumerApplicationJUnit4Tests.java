90b90858f766e77f7f555b30e9473b77d6524ef2
Aaron Gershman <aegershman@gmail.com>
2021-04-17 01:50:35 -0500

    junit54

---
+++ module-consumer/src/test/java/io/slugstack/oss/consumer/ConsumerApplicationJUnit4Tests.java
@@ -0,0 +1,15 @@
+package io.slugstack.oss.consumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+
+public class ConsumerApplicationJUnit4Tests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ module-consumer/src/test/java/io/slugstack/oss/consumer/ConsumerApplicationJUnit5Tests.java
@@ -0,0 +1,11 @@
+package io.slugstack.oss.consumer;
+
+// import org.junit.jupiter.api.Test;
+
+class ConsumerApplicationJUnit5Tests {
+
+    // @Test
+    void contextLoads() {
+    }
+
+}
