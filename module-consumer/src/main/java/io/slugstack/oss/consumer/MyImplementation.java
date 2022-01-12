0dcd8ca1c9608d0b1254120123f558b9285a58a9
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2022-01-11 19:30:13 -0600

    re-eval (#23)

--- module-consumer/src/main/java/io/slugstack/oss/consumer/MyImplementation.java
+++ module-consumer/src/main/java/io/slugstack/oss/consumer/MyImplementation.java
@@ -1,9 +1,14 @@
 package io.slugstack.oss.consumer;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import io.slugstack.oss.library.MyClassToExtend;
 import io.slugstack.oss.library.MyInterface;
 
 public class MyImplementation extends MyClassToExtend implements MyInterface {
+    Logger logger = LogManager.getLogger(MyImplementation.class);
+
     public void someInterfaceMethod() {
         // not used
     }
--- module-standalone/src/main/java/io/slugstack/oss/standalone/Standalone.java
+++ module-standalone/src/main/java/io/slugstack/oss/standalone/Standalone.java
@@ -1,15 +1,21 @@
 package io.slugstack.oss.standalone;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 public class Standalone {
 
 
 
+    Logger logger = LogManager.getLogger(Standalone.class);
 
 
 
 
 
-
+    void method(String name) {
+        logger.info("example " + name);
+    }
 
 
 
